ffad7548b6437b9bcd54a4f981cde470760b2d9b
= <jake@vossen.dev>
2019-03-19 13:06:17 -0600

    created basic request object

--- request.py
+++ request.py
@@ -1,3 +1,13 @@
 class request:
     'This is the main object that stores information about somones request'
-    def__init__(self, t
+    def __init__(self, kind, value, user, date):
+        self.kind = kind # options: "search, url, IPFS hash, git URL
+        self.value = value # the value associated with the type
+        self.user = user # The user associated with this request. We can make a user object later if we feel like it
+        self.date = date # This should be a date-time object
+        self.downloaded_status = False # This is not included in the constructor because state should always be false when starting
+        self.file_location = "" # There is no downloaded location when the object is created
+    def set_downloaded_status(self, state):
+        self.state = state # Should be a boolean
+    def set_file_location(self, loc):
+        self.location = loc # Is there a file object that would be better to use than a string?
