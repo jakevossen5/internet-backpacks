93411a2ed53901ca26f8031f05f1d55960f9f734
= <jake@vossen.dev>
2019-03-26 16:26:58 -0600

    can get the title from a webpage, added UUID to request

--- CommandAndControl.py
+++ CommandAndControl.py
@@ -3,22 +3,27 @@ from user import user
 from datetime import datetime
 import urllib.request
 import urllib.parse
-
+import re
 def download_all_requests(requests):
     for r in requests:
         download_request(r)
 
 def download_request(r):
-    if r.kind == "URL": # we are dealing with a plain old HTTP file
+    if r.kind == "URL": # we are dealing with a plain old HTTP request
         url = r.value
         f = urllib.request.urlopen(url)
-        print(f.read().decode('ISO-8859-1'))
+        regex = re.compile('<title>(.*?)</title>', re.IGNORECASE|re.DOTALL)
+        title = regex.search(f.read().decode('ISO-8859-1')).group(1)
+        print("title:", title)
+
+    # file = open("/tmp/test.html","w")
+    # file.write(str(f.read()))
     
 def main():
     requests = []
     requests.append(Request("URL", "https://www.google.com/", user("Jake", "Vossen", "jakevossen", "asdf"), datetime.now()))
     requests.append(Request("URL", "https://en.wikipedia.org/wiki/Monty_Python_and_the_Holy_Grail", user("Jake", "Vossen", "jakevossen", "asdf"), datetime.now()))
-
+    
     download_all_requests(requests)
 
 main()
--- request.py
+++ request.py
@@ -1,3 +1,4 @@
+import uuid
 class Request:
     'This is the main object that stores information about somones request'
     def __init__(self, kind, value, user, date):
@@ -6,6 +7,7 @@ class Request:
         self.user = user # The user associated with this request. We can make a user object later if we feel like it
         self.date = date # This should be a date-time object
         self.downloaded_status = False # This is not included in the constructor because state should always be false when starting
+        self.uuid=str(uuid.uuid4())
         self.file_location = "" # There is no downloaded location when the object is created
     def set_downloaded_status(self, state):
         self.state = state # Should be a boolean
