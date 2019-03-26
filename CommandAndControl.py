1a65b4461cf180998262bc7d201eb9fa5a9943bc
= <jake@vossen.dev>
2019-03-26 16:34:53 -0600

    wrting files based off the title of their webpage

--- CommandAndControl.py
+++ CommandAndControl.py
@@ -12,15 +12,19 @@ def download_request(r):
     if r.kind == "URL": # we are dealing with a plain old HTTP request
         url = r.value
         f = urllib.request.urlopen(url)
+        html_contents = f.read().decode('ISO-8859-1')
         regex = re.compile('<title>(.*?)</title>', re.IGNORECASE|re.DOTALL)
-        title = regex.search(f.read().decode('ISO-8859-1')).group(1)
+        title = regex.search(html_contents).group(1)
         print("title:", title)
+        file = open("output/" + title + ".html","w")
+        file.write(html_contents)
 
     # file = open("/tmp/test.html","w")
     # file.write(str(f.read()))
     
 def main():
     requests = []
+    # tests
     requests.append(Request("URL", "https://www.google.com/", user("Jake", "Vossen", "jakevossen", "asdf"), datetime.now()))
     requests.append(Request("URL", "https://en.wikipedia.org/wiki/Monty_Python_and_the_Holy_Grail", user("Jake", "Vossen", "jakevossen", "asdf"), datetime.now()))
     
