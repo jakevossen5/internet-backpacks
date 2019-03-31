e0aca08b1e877d33dd16fd194003effc06b0469f
William Culver <williamculver51@gmail.com>
2019-03-31 16:04:23 -0600

    Add files via upload
    ID is now a uuid

--- user.py
+++ user.py
@@ -1,7 +1,8 @@
+import uuid
 class user:
     def __init__(self, first_name, last_name, username, password):
-        self.ID = ""  # ID not needed for inserting into database and can be mapped to the object later
+        self.ID = str(uuid.uuid4())
         self.first_name = first_name
         self.last_name = last_name
         self.username = username
-        self.password = password  # The real thing would have hashed passwords but we probably don't need them for our purposes
+        self.password = password  # in hashed form
