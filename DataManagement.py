90d0df96033dc3b151740bbe5f9b296f9d99a5c0
William Culver <williamculver51@gmail.com>
2019-03-31 16:08:49 -0600

    Added password hashing and other improvements

--- DataManagement.py
+++ DataManagement.py
@@ -1,86 +1,112 @@
 import psycopg2
 import datetime
-from request import request
+from passlib.hash import pbkdf2_sha256
+from request import Request
 from user import user
 
-# connect to the database
-connection = psycopg2.connect(
-    host = "",  # depends on host
-    database = "internetdata",
-    user = "postgres",
-    password = "",
-    port = "5432"
-)
-
 current_user = user(None, None, None, None)
+user_request = Request(None, None, None, None)
+
+
+def connect():
+    global connection
+    # connect to the database
+    connection = psycopg2.connect(
+        host="127.0.0.1",  # depends on host
+        database="internetdata",
+        user="postgres",
+        password="uD4ph2stu",
+        port="5432"
+    )
 
 
 def add_request(kind, value, user):
-    # cursor
+    global user_request
+    connect()
+    # cursor for database
     cursor = connection.cursor()
     now = datetime.datetime.now()
     # create a request object and add data to database
-    user_request = request(kind, value, user, now)
-    cursor.execute("INSERT INTO requests VALUES (%s, %s, %s, %s)", (user_request.kind, user_request.value,
-                                                                    user_request.user.ID, now))
+    user_request.__init__(kind, value, user, now)
+    cursor.execute("INSERT INTO requests (\"UUID\", \"User ID\", \"Kind\", \"Value\", \"Download Status\", \"Date\")"
+                   "VALUES (%s, %s, %s, %s, \'False\', %s)",
+                   (user_request.uuid, user_request.user.ID, user_request.kind, user_request.value, now))
     # make changes and close the cursor
     connection.commit()
     cursor.close()
+    connection.close()
 
 
 def get_all_requests():
+    connect()
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM requests")
     rows = cursor.fetchall()
     cursor.close()
+    connection.close()
     # returns a tuple where the first index is the row and the second index is the column
     return rows
 
 
 # I added the user_request parameter so you have to specify which request to update
-def update_request(user_request, status, location):
-    user_request.set_downloaded_status(status)
-    user_request.set_file_location(location)
+def update_request(current_request, status, location):
+    connect()
+    current_request.set_downloaded_status(status)
+    current_request.set_file_location(location)
+    cursor = connection.cursor()
+    cursor.execute(f"UPDATE requests SET \"Download Status\" = \'{current_request.downloaded_status}\', "
+                   f"\"File Location\" = \'{current_request.file_location}\' WHERE \"UUID\" = \'{current_request.uuid}\'")
+    connection.commit()
+    cursor.close()
+    connection.close()
 
 
 # I added this function
 # This creates a new user from the user object and inserts the data into the users table
 # postgreSQL throws an error if a username is already taken so I didn't add anther check here for now
 def new_user(first_name, last_name, username, password):
+    global hash
+    connect()
     cursor = connection.cursor()
-    current_user = user(first_name, last_name, username, password)
-    cursor.execute("INSERT INTO users (\"First Name\", \"Last Name\", \"Username\", \"Password\") "
-                   "VALUES (%s, %s, %s, %s)",
-                   (current_user.first_name, current_user.last_name, current_user.username, current_user.password))
+    # password is hashed
+    hash = pbkdf2_sha256.encrypt(password)
+    current_user = user(first_name, last_name, username, hash)
+    # first name, last name, and username converted to lowercase so logging in is not case sensitive
+    cursor.execute(f"INSERT INTO users (\"ID\", \"First Name\", \"Last Name\", \"Username\", \"Password\") "
+                   f"VALUES (\'{current_user.ID}\', \'{current_user.first_name.lower()}\', "
+                   f"\'{current_user.last_name.lower()}\', \'{current_user.username.lower()}\', \'{current_user.password}\')")
     connection.commit()
     cursor.close()
+    connection.close()
 
 
 # I added this function
 # This checks if the username and password are correct in order to login
-# It then creates a new user which can be used throughout the program for different purposes
-def login():
+# It then creates a new user object which can be used throughout the program for different purposes
+def login(username, password):
+    connect()
     cursor = connection.cursor()
-    username = input("Username: ")
-    password = input("Password: ")
-    cursor.execute(f"SELECT * FROM users WHERE \"Username\" = \'{username}\'")
+    # entered username is converted to lower case to match what is in the database
+    cursor.execute(f"SELECT * FROM users WHERE \"Username\" = \'{username.lower()}\'")
     rows = cursor.fetchall()
-    if rows[0][4] == password:
-        print("Login Success")
-        ID = rows[0][0]
+    hash = rows[0][4]
+    cursor.close()
+    connection.close()
+    # the entered password is compared to the database password using the hash and encryption algorithm
+    if pbkdf2_sha256.verify(password, hash):
+        ID = str(rows[0][0])
         create_user_object(ID)
-        cursor.close()
         return True
     else:
-        print("Incorrect Password. Try Again.")
         return False
 
 
 # Creates a user object for the current user so that their data can be accessed easily
-def create_user_object(id_num):
+def create_user_object(uuid_num):
     global current_user
+    connect()
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM users WHERE \"ID\" = %s", str(id_num))
+    cursor.execute(f"SELECT * FROM users WHERE \"ID\" = \'{uuid_num}\'")
     rows = cursor.fetchall()
     current_user.ID = rows[0][0]
     current_user.first_name = rows[0][1]
@@ -88,25 +114,4 @@ def create_user_object(id_num):
     current_user.username = rows[0][3]
     current_user.password = rows[0][4]
     cursor.close()
-
-
-# Login loop for testing purposes
-
-login_success = login()
-
-while not login_success:
-    login_success = login()
-    if login_success:
-        break
-
-# # more testing stuff
-# fn = input("Enter your first name: ")
-# ln = input("Enter your last name: ")
-# un = input("Enter a username: ")
-# pw = input("Enter a password: ")
-#
-# new_user(fn, ln, un, pw)
-
-
-# closes database connection
-connection.close()
+    connection.close()
