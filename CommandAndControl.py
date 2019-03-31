238322ab1bdac1a8605032b86076e1b48ed3d0f9
= <jake@vossen.dev>
2019-03-30 22:43:10 -0600

    added ipfs

--- CommandAndControl.py
+++ CommandAndControl.py
@@ -10,8 +10,10 @@ def download_all_requests(requests):
     for r in requests:
         download_request(r)
 
+def mkdir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)    
 def download_request(r):
-
     if r.kind == "URL": # we are dealing with a plain old HTTP request
         url = r.value
         path = "output/" + r.uuid
@@ -28,31 +30,32 @@ def download_request(r):
     if r.kind == "youtube":
         path = "output/" + r.uuid + '/'
         download_from_youtube(r.value, path)
+    if r.kind == "ipfs":
+        path = "output/" + r.uuid + '/'
+        download_from_ipfs(r.value, path)
 
-    # file = open("/tmp/test.html","w")
-    # file.write(str(f.read()))
 def download_from_url(url, path):
-    if not os.path.exists(path):
-        os.makedirs(path)
-        
+    mkdir(path)
     subprocess.call(r'wget -E -H -k -K -p ' '-P ' + path + ' ' +  url + ' robots=off', shell=True)
 def download_from_youtube(url, path):
-    if not os.path.exists(path):
-        os.makedirs(path)
+    mkdir(path)
     ydl_opts = {
         'outtmpl': path + '%(title)s'
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
+def download_from_ipfs(ipfs_hash, path):
+    mkdir(path)
+    subprocess.call(r'ipfs get ' + ipfs_hash + ' -o ' + path, shell=True)
 def main():
     requests = []
     # tests
-    requests.append(Request("URL", "https://www.google.com/", user("Jake", "Vossen", "jakevossen", "asdf"), datetime.now()))
-    requests.append(Request("URL", "https://static.googleusercontent.com/media/research.google.com/en//pubs/archive/46507.pdf", user("Jake", "Vossen", "jakevossen", "asdf"), datetime.now()))
-    requests.append(Request("URL", "https://en.wikipedia.org/wiki/Monty_Python_and_the_Holy_Grail", user("Jake", "Vossen", "jakevossen", "asdf"), datetime.now()))
-    requests.append(Request("search", "What Is the Airspeed Velocity of an Unladen Swallow?", user("Jake", "Vossen", "jakevossen", "asdf"), datetime.now()))
-    requests.append(Request("youtube", "https://www.youtube.com/watch?v=dQw4w9WgXcQ", user("Jake", "Vossen", "jakevossen", "asdf"), datetime.now()))
-
+    # requests.append(Request("URL", "https://www.google.com/", user("Jake", "Vossen", "jakevossen", "asdf"), datetime.now()))
+    # requests.append(Request("URL", "https://static.googleusercontent.com/media/research.google.com/en//pubs/archive/46507.pdf", user("Jake", "Vossen", "jakevossen", "asdf"), datetime.now()))
+    # requests.append(Request("URL", "https://en.wikipedia.org/wiki/Monty_Python_and_the_Holy_Grail", user("Jake", "Vossen", "jakevossen", "asdf"), datetime.now()))
+    # requests.append(Request("search", "What Is the Airspeed Velocity of an Unladen Swallow?", user("Jake", "Vossen", "jakevossen", "asdf"), datetime.now()))
+    # requests.append(Request("youtube", "https://www.youtube.com/watch?v=dQw4w9WgXcQ", user("Jake", "Vossen", "jakevossen", "asdf"), datetime.now()))
+    requests.append(Request("ipfs", "/ipfs/QmS4ustL54uo8FzR9455qaxZwuMiUhyvMcX9Ba8nUH4uVv/readme", user("Jake", "Vossen", "jakevossen", "asdf"), datetime.now()))    
     
     download_all_requests(requests)
 
