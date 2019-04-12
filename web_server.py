785533ca6201d3dfde0be2d93eb3d20170075543
= <jake@vossen.dev>
2019-04-12 16:04:13 -0600

    getting ready to implement gui

--- web_server.py
+++ web_server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, flash, request
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
- 
+import CommandAndControl 
 # App config.
 DEBUG = True
 app = Flask(__name__)
@@ -9,8 +9,8 @@ app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
  
 class ReusableForm(Form):
     name = TextField('Name:', validators=[validators.required()])
-    email = TextField('Email:', validators=[validators.required(), validators.Length(min=6, max=35)])
-    password = TextField('Password:', validators=[validators.required(), validators.Length(min=3, max=35)])
+    email = TextField('Email:')#, validators=[validators.required(), validators.Length(min=6, max=35)])
+    # password = TextField('Password:', validators=[True])
  
 @app.route("/", methods=['GET', 'POST'])
 def hello():
@@ -19,9 +19,8 @@ def hello():
     print (form.errors)
     if request.method == 'POST':
         name=request.form['name']
-        password=request.form['password']
         email=request.form['email']
-        print (name, " ", email, " ", password)
+        print (name, " ", email)
  
     if form.validate():
 # Save the comment here.
@@ -30,6 +29,25 @@ def hello():
         flash('Error: All the form fields are required. ')
  
     return render_template('hello.html', form=form)
+
+@app.route("/google-research", methods=['GET', 'POST'])
+def googleresearch():
+    form = ReusableForm(request.form)
  
+    print (form.errors)
+    if request.method == 'POST':
+        name=request.form['name']
+        email=request.form['email']
+        print (name, " ", email)
+ 
+    if form.validate():
+# Save the comment here.
+        flash('Thanks for registration ' + name)
+
+        
+    else:
+        flash('Error: All the form fields are required. ')
+ 
+    return render_template('research.html', form=form) 
 if __name__ == "__main__":
     app.run()
